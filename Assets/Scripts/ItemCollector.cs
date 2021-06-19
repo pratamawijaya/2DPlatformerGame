b2a17a964fa96b94755f04e93ecf2b0caab1718d
pratamawijaya <set.mnemonix@gmail.com>
2021-06-19 21:58:41 +0700

    now we have prefab, and we have cherry collector counter

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class ItemCollector : MonoBehaviour
 {
+    private int cherries = 0;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Cherry"))
         {
             Destroy(collision.gameObject);
+            cherries++;
+            Debug.Log("our cherry :" + cherries);
         }
     }
     // Start is called before the first frame update
